ec73e965056efb2e14ad9d0f45d0f3024377389a
James Lemire <webdev.jameslemire@gmail.com>
2017-06-09 11:41:40 -0600

    JL master - Update initial files.

--- src/utils/weatherUtils.js
+++ src/utils/weatherUtils.js
@@ -4,9 +4,8 @@ import rainy from "../assets/rainy.svg";
 import snowy from "../assets/snowy.svg";
 import sunny from "../assets/sunny.svg";
 import unknownIcon from "../assets/unknown-icon.svg";
-import API_KEY from "../apiKey";
 
-const BASE_URL = `http://api.openweathermap.org/data/2.5/weather?APPID=${ API_KEY }&units=imperial&`;
+const BASE_URL = ``;
 function isZipCode( location ) { return !isNaN( parseInt( location ) ); }
 function getWeatherIcon( conditionCode ) { if ( conditionCode === 800 ) { return sunny; } if ( conditionCode >= 200 && conditionCode < 600 ) { return rainy; } if ( conditionCode >= 600 && conditionCode < 700 ) { return snowy; } if ( conditionCode >= 801 && conditionCode <= 803 ) { return partlyCloudy; } if ( conditionCode === 804 ) { return cloudy; } return unknownIcon; }
 export function formatWeatherData( weatherData ) { return { icon: getWeatherIcon( weatherData.weather[ 0 ].id ), currentTemperature: weatherData.main.temp, location: weatherData.name, maxTemperature: weatherData.main.temp_max, minTemperature: weatherData.main.temp_min, humidity: weatherData.main.humidity, wind: weatherData.wind.speed }; }
