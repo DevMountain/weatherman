889203d4ee4951acb9c7395dbc860e01bc8d018d
James Lemire <webdev.jameslemire@gmail.com>
2017-06-09 11:17:00 -0600

    JL master - Update initial files

--- src/ducks/weather.js
+++ src/ducks/weather.js
@@ -1,19 +1,19 @@
-const RESET = "RESET";
-
 const initialState = {
-	  error: false
-	, loading: false
-	, search: true
-	, weather: {}
+  error: false,
+  loading: false,
+  search: true,
+  weather: {}
 };
 
+const RESET = "RESET";
+
 export default function weather( state = initialState, action ) {
-	switch ( action.type ) {
-		case RESET: return initialState;
-		default: return state;
-	}
+  switch ( action.type ) {
+    case RESET: return initialState;
+    default: return state;
+  }
 }
 
 export function reset() {
-	return { type: RESET };
+  return { type: RESET };
 }
--- src/utils/weatherUtils.js
+++ src/utils/weatherUtils.js
@@ -5,46 +5,6 @@ import snowy from "../assets/snowy.svg";
 import sunny from "../assets/sunny.svg";
 import unknownIcon from "../assets/unknown-icon.svg";
 
-function isZipCode( location ) {
-	return !isNaN( parseInt( location ) );
-}
-
-function getWeatherIcon( conditionCode ) {
-	if ( conditionCode === 800 ) {
-		return sunny;
-	}
-
-	if ( conditionCode >= 200 && conditionCode < 600 ) {
-		return rainy;
-	}
-
-	if ( conditionCode >= 600 && conditionCode < 700 ) {
-		return snowy;
-	}
-
-	if ( conditionCode >= 801 && conditionCode <= 803 ) {
-		return partlyCloudy;
-	}
-
-	if ( conditionCode === 804 ) {
-		return cloudy;
-	}
-
-	return unknownIcon;
-}
-
-export function formatWeatherData( weatherData ) {
-	return {
-		  icon: getWeatherIcon( weatherData.weather[ 0 ].id )
-		, currentTemperature: weatherData.main.temp
-		, location: weatherData.name
-		, maxTemperature: weatherData.main.temp_max
-		, minTemperature: weatherData.main.temp_min
-		, humidity: weatherData.main.humidity
-		, wind: weatherData.wind.speed
-	};
-}
-
-export function buildUrl( location ) {
-
-}
+function isZipCode( location ) { return !isNaN( parseInt( location ) ); }
+function getWeatherIcon( conditionCode ) { if ( conditionCode === 800 ) { return sunny; } if ( conditionCode >= 200 && conditionCode < 600 ) { return rainy; } if ( conditionCode >= 600 && conditionCode < 700 ) { return snowy; } if ( conditionCode >= 801 && conditionCode <= 803 ) { return partlyCloudy; } if ( conditionCode === 804 ) { return cloudy; } return unknownIcon; }
+export function formatWeatherData( weatherData ) { return { icon: getWeatherIcon( weatherData.weather[ 0 ].id ), currentTemperature: weatherData.main.temp, location: weatherData.name, maxTemperature: weatherData.main.temp_max, minTemperature: weatherData.main.temp_min, humidity: weatherData.main.humidity, wind: weatherData.wind.speed }; }
