f1f4303dc708917aa6c2772a32902be3f9e5fc14
James Lemire <webdev.jameslemire@gmail.com>
2017-06-09 12:36:36 -0600

    JL solution

--- src/components/EnterLocation/EnterLocation.js
+++ src/components/EnterLocation/EnterLocation.js
@@ -1,50 +1,51 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { setWeather } from '../../ducks/weather';
 
 import "./EnterLocation.css";
 
-import { getWeather } from "../../services/weatherService";
+class EnterLocation extends Component {
+  constructor( props ) {
+    super( props );
 
-export default class EnterLocation extends Component {
-	constructor( props ) {
-		super( props );
+    this.state = { location: "" };
 
-		this.state = { location: "" };
+    this.handleChange = this.handleChange.bind( this );
+    this.handleSubmit = this.handleSubmit.bind( this );
+  }
 
-		this.handleChange = this.handleChange.bind( this );
-		this.handleSubmit = this.handleSubmit.bind( this );
-	}
+  handleChange( event ) {
+    this.setState( { location: event.target.value } );
+  }
 
-	handleChange( event ) {
-		this.setState( { location: event.target.value } );
-	}
+  handleSubmit( event ) {
+    event.preventDefault();
+    this.props.setWeather( this.state.location )
+    this.setState( { location: "" } );
+  }
 
-	handleSubmit( event ) {
-		event.preventDefault();
-
-		getWeather( this.state.location );
-
-		this.setState( { location: "" } );
-	}
-
-	render() {
-		return (
-			<form
-				className="enter-location"
-				onSubmit={ this.handleSubmit }
-			>
-				<input
-					className="enter-location__input"
-					onChange={ this.handleChange }
-					placeholder="London / 84601"
-					type="text"
-					value={ this.state.location }
-				/>
-				<button
-					className="enter-location__submit"
-				>
-					Submit
-				</button>
-			</form>
-		);
-	}
+  render() {
+    return (
+      <form
+        className="enter-location"
+        onSubmit={ this.handleSubmit }
+      >
+        <input
+          className="enter-location__input"
+          onChange={ this.handleChange }
+          placeholder="London / 84601"
+          type="text"
+          value={ this.state.location }
+        />
+        <button
+          className="enter-location__submit"
+        >
+          Submit
+        </button>
+      </form>
+    );
+  }
 }
+
+export default connect( state => state, { setWeather })( EnterLocation );
