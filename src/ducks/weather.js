b063c39282b0573be9863f9ad5ea4989592c2b23
James Lemire <webdev.jameslemire@gmail.com>
2017-06-09 11:25:38 -0600

    JL solution

--- src/ducks/weather.js
+++ src/ducks/weather.js
@@ -1,45 +1,46 @@
+const initialState = {
+  error: false,
+  loading: false,
+  search: true,
+  weather: {}
+};
+
 const RESET = "RESET";
 const SET_WEATHER = "SET_WEATHER";
 
-const initialState = {
-	  error: false
-	, loading: false
-	, search: true
-	, weather: {}
-};
-
 export default function weather( state = initialState, action ) {
-	switch ( action.type ) {
-		case SET_WEATHER + "_PENDING":
-			return {
-				  error: false
-				, loading: true
-				, search: false
-				, weather: {}
-			};
-		case SET_WEATHER + "_FULFILLED":
-			return {
-				  error: false
-				, loading: false
-				, search: false
-				, weather: action.payload
-			};
-		case SET_WEATHER + "_REJECTED":
-			return {
-				  error: true
-				, loading: false
-				, search: false
-				, weather: {}
-			};
-		case RESET: return initialState;
-		default: return state;
-	}
+  switch ( action.type ) {
+    case SET_WEATHER + "_PENDING":
+      return {
+        error: false,
+        loading: true,
+        search: false,
+        weather: {}
+      };
+    case SET_WEATHER + "_FULFILLED":
+      return {
+        error: false,
+        loading: false,
+        search: false,
+        weather: action.payload
+      };
+    case SET_WEATHER + "_REJECTED":
+      return {
+        error: true,
+        loading: false,
+        search: false,
+        weather: {}
+      };
+      
+    case RESET: return initialState;
+    default: return state;
+  }
 }
 
 export function reset() {
-	return { type: RESET };
+  return { type: RESET };
 }
 
 export function setWeather( weatherPromise ) {
-	return { payload: weatherPromise, type: SET_WEATHER };
+  return { payload: weatherPromise, type: SET_WEATHER };
 }
